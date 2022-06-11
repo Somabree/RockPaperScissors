2450cdc9dfd386369780c7fcee37dc89705d71bc
Somabree <preshy2047@gmail.com>
2022-06-11 21:01:38 +0100

    created authentication for input

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+
+print("Welcome to Rock, Paper, Scissors")
+print("R represents Rock")
+print("P represents Paper")
+print("S represents Scissors")
+
+player_wins = 0
+computer_wins = 0
+
+possible_choices = ["R", "P", "S"]
+
+while True:
+    player_choice = input("Pick a choice from R, P, S: ")
+    player_choice = player_choice.upper().strip()
+    
+    if player_choice not in possible_choices:
+        print("Invalid choice, pick either R, P or S ")
+        continue
+    
+    computer_choice = random.choice(possible_choices)
+    print(f"Player ({player_choice}) :  CPU ({computer_choice})")
+    
+    if player_choice == "R" and computer_choice == "s":
+       print("You win!")
+
+    elif player_choice == "P" and computer_choice == "R":
+        print("You win!")
+
+    elif player_choice == "S" and computer_choice == "P":
+        print("You win!")
+
+    elif player_choice == computer_choice:
+        print("It's a tie, try again")
+        continue
+
+    else:
+        print("Computer wins!")
+        break
+    
+
+
